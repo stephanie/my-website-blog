00617b73474ee829a126b30d87b58ed6c6ecff98
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-04-24 22:27:35 +0800

    added obtvse

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Obtvse::Application.routes.draw do
+
+  # Admin
+  get '/admin' => 'posts#admin', as: 'admin'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+
+  get '/posts(.:format)' => 'posts#index'
+  get '/posts.rss' => 'posts#index', as: 'rss'
+
+  resources :users
+  resources :sessions
+  resources :posts, path: '/'
+
+  get '/:slug' => 'posts#show', as: 'post_slug'
+
+  root to: 'posts#index'
+
+  # Sitemap
+  get "sitemap/sitemap.xml" => "sitemap#index", as: "sitemap", defaults: { format: "xml" }
+end
